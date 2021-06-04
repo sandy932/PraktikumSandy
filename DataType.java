784c1d0548a34be3da94ed10b6327f02eb8e68b2
sandy932 <sandymuaz76@gmail.com>
2021-06-04 19:14:37 +0800

    deklarasi variabel dan tipe data

---
+++ DataType.java
@@ -0,0 +1,13 @@
+public class DataType{
+	public static void main(String[]args){
+	String namaDepan = "SANDY THREE";
+	String namaTengah = "MU'AZ";
+	String namaBelakang = "PUTRA";
+
+	int usia = 21;
+	int targetTahunKuliah = 4;
+	double ipk = 3.5;
+	boolean lulus = true;
+	char nilaiAbjad = 'A';
+ }	
+}
