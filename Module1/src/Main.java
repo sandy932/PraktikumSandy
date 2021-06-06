634a39787fda9afc4faf2ff4515d7280c416b6e2
sandy932 <sandymuaz76@gmail.com>
2021-06-06 10:59:16 +0800

    Operasi Tipe String

--- Module1/src/Main.java
+++ Module1/src/Main.java
@@ -8,11 +8,28 @@ public class Main {
         double ipk = 3.5;
         boolean lulus = true;
         char nilaiAbjad = 'A';
-        char[] uniskaChar = {'U','N','I','S','K','A'};
 
-        String uniskaString = new String(uniskaChar);
-
-        System.out.println(uniskaString);
         System.out.println(namaDepan + namaTengah + namaBelakang);
+
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+        System.out.println(uniskaString);
+
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("NIS"));
+        System.out.println(namaLengkap.substring(5));
+        System.out.println(namaLengkap.substring(5,12));
+        System.out.println(namaLengkap.replace("San","Ba"));
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.charAt(6));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for(String nama : namaArray){
+            System.out.println(nama);
+        }
     }
 }
