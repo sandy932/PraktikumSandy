4afbe5c4f0d5f979bac430dd369e630b671bd8a8
sandy932 <sandymuaz76@gmail.com>
2021-06-06 10:35:03 +0800

    Project IDEA

---
+++ Module1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "SANDY THREE";
+        String namaTengah = "MU'AZ";
+        String namaBelakang = "PUTRA";
+        int usia = 21;
+        int targetTahunKuliah = 4;
+        double ipk = 3.5;
+        boolean lulus = true;
+        char nilaiAbjad = 'A';
+
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
